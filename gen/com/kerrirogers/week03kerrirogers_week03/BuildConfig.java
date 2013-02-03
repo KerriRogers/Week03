7dbfdfaf4f7d50eba4a8d7fb23e28dee3e110c97
Kerri <Kerri@Kerri-PC>
2013-02-02 23:29:24 -0800

    Week 3 Project fodr Prog272. I hope.

---
+++ gen/com/kerrirogers/week03kerrirogers_week03/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.kerrirogers.week03kerrirogers_week03;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
